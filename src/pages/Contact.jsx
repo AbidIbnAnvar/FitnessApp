3c19ec56ecb435d4e3ef1ef84ffbeb49e1cf1538
Abid <121794232+AbidIbnAnvar@users.noreply.github.com>
2023-05-05 13:54:59 +0530

    Add files via upload

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,6 +13,14 @@ function Contact() {
                             location_on</span>
                         </div>
                         <div class="icon-text">Our Location</div>
+                        <div className="box-contents">
+                        <ul>
+                            <li>National Institute of Technology</li>
+                            <li>P.O 673601</li>
+                            <li>Kattangal</li>
+                            <li>Calicut</li>
+                        </ul>
+                    </div>  
                         
                     </div>
                     <div class="contact2">
@@ -20,12 +28,28 @@ function Contact() {
                             call</span>
                         </div>
                         <div class="icon-text">Call us</div>
+                        <div className="box-contents">
+                        <ul>
+                            <li>+91 7418950210</li>
+                            <li>+91 6238873294</li>
+                            <li>+91 8592915952</li>
+                            <li>+91 8129374789</li>
+                        </ul>
+                    </div>  
                     </div>
                     <div class="contact3">
                         <div class="icons"><span class="material-symbols-rounded">
                             mail</span>
                         </div>
                         <div class="icon-text">Email us</div>
+                        <div className="box-contents">
+                        <ul>
+                            <li>abid_b220004cs@nitc.ac.in</li>
+                            <li>adil_b220005ee@nitc.ac.in</li>
+                            <li>ajmalrishdin@gmail.com</li>
+                            <li>jaefarshameem@gmail.com</li>
+                        </ul>
+                    </div>  
                     </div>
                 </div>
             </div>
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -9,17 +9,42 @@ function Pricing() {
             <div className="boxes">
                 <div className="box1">
                     <h2 className="box-heading">BASIC</h2>
-                    <div className="price-container"><h6><div className="price">$39</div>/MONTH</h6></div>
+                    <div className="price-container"><h6><div className="price">$19</div>/MONTH</h6></div>
+                    <div className="box-contents">
+                      <ul>
+                        <li>Point 1</li>
+                        <li className='not-available'>Point 2</li>
+                        <li className='not-available'>Point 3</li>
+                        <li className="not-available">Point 3</li>
+                      </ul>
+                    </div>
                 </div>
                 <div className="box2">
-                    <h2 className="box-heading">PLUS</h2>
-                    
-                    <div className="price-container"><h6><div className="price">$59</div>/MONTH</h6></div>
+                    <h2 className="box-heading">PLUS </h2>
+                    <div className="price-container"><h6><div className="price">$29</div>/MONTH</h6></div>
+                    <div className="box-contents">
+                      <ul>
+                        <li>Point 1</li>
+                        <li>Point 2</li>
+                        <li>Point 3</li>
+                        <li className="not-available">Point 3</li>
+                      </ul>
+                    </div>
+                    <div className="best-value">Best Value</div>
                 </div>
 
                 <div className="box3">
                     <h2 className="box-heading">PRO</h2>
-                    <div className="price-container"><h6><div className="price">$89</div>/MONTH</h6></div>
+                    <div className="price-container"><h6><div className="price">$49</div>/MONTH</h6></div>
+                    <div className="box-contents">
+                      <ul>
+                        <li>Point 1</li>
+                        <li>Point 2</li>
+                        <li>Point 3</li>
+                        <li>Point 3</li>
+                      </ul>
+                    </div>
+                    
                 </div>
                 
                 
